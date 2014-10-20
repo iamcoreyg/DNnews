8ad7e7e7547bb834efa4ea14a32733a0bbf39b47
iamcoreyg <iamcoreyg@gmail.com>
2014-10-20 12:53:17 -0700

    Initial Commit

---
+++ designcode-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  designcode-1
+//
+//  Created by Corey Griffin on 10/20/14.
+//  Copyright (c) 2014 Corey Griffin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
